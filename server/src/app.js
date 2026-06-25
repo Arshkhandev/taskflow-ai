fcd270897d3523046aa39d9b0479db9decab33bb
Arshkhandev <arsh.khan.dev@gmail.com>
2026-06-25 09:47:33 +0530

    taskflow ai initilize

---
+++ server/src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+
+// Health Check Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "TaskFlow AI Backend Running",
+  });
+});
+
+export default app;
---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config();
+
+
+const PORT = process.env.port || 5000 ;
+
+app.listen(PORT , () => {
+     console.log(`app running in the http://localhost:${PORT}`);
+})
